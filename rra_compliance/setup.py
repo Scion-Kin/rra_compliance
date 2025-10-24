19eb38120e49785aa815865f577791d6f428987c
Mugabo Gusenga <dinturner17@gmail.com>
2025-10-24 15:44:25 +0200

    Forgot commas

--- rra_compliance/setup.py
+++ rra_compliance/setup.py
@@ -99,7 +99,7 @@ class RRAComplianceFactory:
 				"taxes": {
 					"eval": """
 {
-	"tax_type": frappe.get_last_doc("Account", filters={"name": ["like", "VAT - %"]}).name
+	"tax_type": frappe.get_last_doc("Account", filters={"title": ["like", "VAT - %"]}).name,
 	"tax_rate": 18 if i.get("cd") == "B" else 0
 }
 		"""
