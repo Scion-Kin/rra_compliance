7a3d5591d9caa98e99781138b18008be825e5b95
Mugabo Gusenga <dinturner17@gmail.com>
2025-10-29 12:28:22 +0200

    Add necessary doctypes for invoice transactions

---
+++ rra_compliance/rra_compliance/doctype/rra_sales_invoice_log/rra_sales_invoice_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Buffer Punk and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("RRA Sales Invoice Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
