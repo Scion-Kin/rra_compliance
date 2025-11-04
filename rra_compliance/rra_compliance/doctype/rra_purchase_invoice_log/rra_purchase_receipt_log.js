674c007350a0585b173d26f9a8bf3f9ba8dc725a
Mugabo Gusenga <dinturner17@gmail.com>
2025-11-04 11:55:26 +0200

    Remove problematic doc

--- rra_compliance/rra_compliance/doctype/rra_purchase_invoice_log/rra_purchase_receipt_log.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Buffer Punk and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("RRA Purchase Receipt Log", {
-// 	refresh(frm) {
-
-// 	},
-// });
