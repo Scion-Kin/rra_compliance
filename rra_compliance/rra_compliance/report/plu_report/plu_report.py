08c610f09b7a5afb4f2426707fd9c5381ed3c9bc
Mugabo Gusenga <dinturner17@gmail.com>
2026-02-17 15:04:46 +0200

    FEAT: Add PLU Report with date filters and item details

---
+++ rra_compliance/rra_compliance/report/plu_report/plu_report.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2025, Buffer Punk Ltd and contributors
+# For license information, please see license.txt
+
+import frappe
+
+
+def execute(filters={}):
+	columns = [
+		{ "fieldname": "sales_invoice", "label": "Sales Invoice", "fieldtype": "Link", "options": "Sales Invoice", "width": 150 },
+		{ "fieldname": "itemCd", "label": "Item Code", "fieldtype": "Data", "width": 150 },
+		{ "fieldname": "itemNm", "label": "Item Name", "fieldtype": "Data", "width": 200 },
+		{ "fieldname": "prc", "label": "Unit Price", "fieldtype": "Currency", "width": 150 },
+		{ "fieldname": "totAmt", "label": "Total Amount", "fieldtype": "Currency", "width": 150 },
+		{ "fieldname": "taxAmt", "label": "Tax Amount", "fieldtype": "Currency", "width": 150 },
+		{ "fieldname": "qty", "label": "Quantity Sold", "fieldtype": "Float", "width": 150 },
+		{ "fieldname": "stock_qty", "label": "Remaining Stock Quantity", "fieldtype": "Float", "width": 150 },
+	]
+
+	sales_invoices = frappe.db.get_all("Sales Invoice", filters={"docstatus": 1, "posting_date": ["Between", [filters.get("start_date"), filters.get("end_date")]]}, pluck="name")
+	logs = frappe.db.get_all("RRA Sales Invoice Log", filters={"docstatus": 1, "sales_invoice": ["in", sales_invoices]}, fields=["*"])
+
+	items = []
+	for log in logs:
+		log_items = frappe.parse_json(log.get("payload", {})).get("itemList", [])
+		for item in log_items:
+			legder_qty = frappe.db.get_value("Stock Ledger Entry", {"voucher_no": log.sales_invoice, "item_code": item.get("itemCd")}, "actual_qty_after_transaction")
+			items.append(item.update({"sales_invoice": log.sales_invoice, "stock_qty": legder_qty}))
+
+	return columns, items
+
